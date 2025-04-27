6c29ba4244f9d994475a340591900b4a5f0f1b07
Nicholas Devenish <ndevenish@gmail.com>
2025-04-27 17:01:06 +0100

    Try to fix missing files in distribution

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,8 @@
 
 export SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION"
 echo "version = \"$PKG_VERSION\"" >src/build123d/_version.py
+# Running python build on the sources works, but these are missed in conda-build?
+echo "include src/build123d/template_render.js" >> MANIFEST.in
+echo "include src/build123d/py.typed" >> MANIFEST.in
+
 "$PYTHON" -m pip install . -vv --no-deps --no-build-isolation
